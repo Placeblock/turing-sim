34b1474edec47b521c8c7a279ddb3b0fd03ec6e2
LordElias <kerlinelias@gmail.com>
2025-06-11 16:05:19 +0200

    add getters to the data classes

--- core/State.java
+++ core/State.java
@@ -7,6 +7,19 @@ import java.util.Map;
  * A state consists of transitions for each character, and a flag indicating if it is a terminating state.
  */
 public class State {
-    private Map<Character, Transition> transitions;
-    private boolean terminates;
+    private final Map<Character, Transition> transitions;
+    private final boolean terminates;
+
+    public State(Map<Character, Transition> transitions, boolean terminates) {
+        this.transitions = transitions;
+        this.terminates = terminates;
+    }
+
+    public Map<Character, Transition> getTransitions() {
+        return transitions;
+    }
+
+    public boolean isTerminating() {
+        return terminates;
+    }
 }
--- core/Transition.java
+++ core/Transition.java
@@ -5,7 +5,25 @@ package core;
  * A transition consists of a new state, a new symbol to write, and a move direction.
  */
 public class Transition {
-    private State newState;
-    private char newSymbol;
-    private Move move;
+    private final char newSymbol;
+    private final Move move;
+    private final State newState;
+
+    public Transition(char newSymbol, Move move, State newState) {
+        this.newSymbol = newSymbol;
+        this.move = move;
+        this.newState = newState;
+    }
+
+    public char getNewSymbol() {
+        return newSymbol;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    public State getNewState() {
+        return newState;
+    }
 }
