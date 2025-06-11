815b62ba9faf0aee24b46003f8cf3268ded209a7
LordElias <kerlinelias@gmail.com>
2025-06-11 13:47:47 +0200

    Add menu bar

--- ui/MainWindow.java
+++ ui/MainWindow.java
@@ -5,6 +5,9 @@ import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import java.awt.FlowLayout;
 
@@ -14,6 +17,15 @@ public class MainWindow extends JFrame {
         setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // Center the window
+
+        // Menu bar setup
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem loadCsvItem = new JMenuItem("Load CSV");
+        loadCsvItem.addActionListener(_ -> openFileChooser());
+        fileMenu.add(loadCsvItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
 
         JButton openButton = new JButton("Open File");
         openButton.addActionListener(_ -> openFileChooser());
