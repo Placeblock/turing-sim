45084b7b2c5154a26fdb319155fb3599d9a6529a
LordElias <kerlinelias@gmail.com>
2025-06-12 11:08:09 +0200

    Serializer add javadoc, handle case for no transitions

--- serialization/Serializer.java
+++ serialization/Serializer.java
@@ -1,15 +1,33 @@
 package serialization;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 import core.State;
 
 public class Serializer {
-    public static void serialize(List<State> states, BufferedOutputStream outputStream) throws IOException {
+    
+    /**
+     * Serializes a list of states to a specified output stream.
+     * Each state is represented by its transitions and whether it is a terminating state.
+     *
+     * @param states the list of states to serialize
+     * @param outputStream the output stream to write the serialized data to
+     * @throws IOException if an I/O error occurs during stream writing
+     */
+    public static void serialize(List<State> states, OutputStream outputStream) throws IOException {
         int i = 0;
         for (var state : states) {
+            if(state.getTransitions().isEmpty()) {
+                String line = String.format("%d,,,,,%b\n",
+                        i,
+                        state.isTerminating()
+                    );
+                outputStream.write(line.getBytes());
+                i++;
+                continue;
+            }
             for (var entry : state.getTransitions().entrySet()) {
                 var symbol = entry.getKey();
                 var transition = entry.getValue();
