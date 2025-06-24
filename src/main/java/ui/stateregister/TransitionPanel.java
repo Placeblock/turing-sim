95094bfa6c36757ee2bdadc9969a666b5e67fe71
David H. <dshuettemeister@gmail.com>
2025-06-25 00:43:31 +0200

    add default for stateComboBox

--- src/main/java/ui/stateregister/TransitionPanel.java
+++ src/main/java/ui/stateregister/TransitionPanel.java
@@ -1,6 +1,9 @@
 package ui.stateregister;
 
+import core.Move;
+import core.State;
 import core.StateRegister;
+import core.Transition;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,22 +11,33 @@ import java.util.Set;
 
 public class TransitionPanel extends JPanel {
     private final StateRegister stateRegister;
-    private Set tapeAlphabet;
+    private Set<Character> tapeAlphabet;
 
-    public TransitionPanel (StateRegister stateRegister, int index){
+    public TransitionPanel (StateRegister stateRegister, Transition transition){
         this.stateRegister = stateRegister;
 
         JPanel panel = new JPanel(new FlowLayout((FlowLayout.LEFT)));
         JComboBox stateComboBox = new JComboBox<>();
         for (int i = 0; i < stateRegister.getStates().size(); i++) {
-            stateComboBox.addItem(stateRegister.getStates().get(i));
+            stateComboBox.addItem(i);
+            // stateComboBox.addItem(stateRegister.getStates().get(i));
+        }
+        State newState = transition.getNewState();
+        stateComboBox.setSelectedItem(stateRegister.getStates().indexOf(newState));
+        // (->q1/q2/q3/q4, B, R)
+        JComboBox symbolComboBox = new JComboBox();
+        for(Character alphabetChar: tapeAlphabet){
+            symbolComboBox.addItem(alphabetChar);
         }
 
-        JComboBox symbolComboBox = new JComboBox();
-
         JComboBox moveComboBox = new JComboBox();
+        for (Move move: Move.values()) {
+            moveComboBox.addItem(move.toString());
+        }
 
-
-    
+        panel.add(stateComboBox);
+        panel.add(symbolComboBox);
+        panel.add(moveComboBox);
     }
+
 }
