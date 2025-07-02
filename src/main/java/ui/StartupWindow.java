90da9e94c564b5dc898328f9a2f747dc0c175114
LordElias <kerlinelias@gmail.com>
2025-07-02 21:19:02 +0200

    refactor StartupWindow: enhance UI components and add file loading functionality

--- src/main/java/ui/StartupWindow.java
+++ src/main/java/ui/StartupWindow.java
@@ -2,11 +2,21 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class StartupWindow extends JFrame {
+
+    private JLabel tapeLabel;
+    private JLabel transitionsLabel;
+    private JButton startButton;
+
+    private boolean isTapeLoaded = false;
+    private boolean areTransitionsLoaded = false;
+
     public StartupWindow() {
         setTitle("Turing Machine Simulator");
-        setSize(300, 150);
+        setSize(400, 200);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // Center the window
@@ -18,22 +28,32 @@ public class StartupWindow extends JFrame {
         // Load Tape button and label
         JPanel tapePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton loadTapeButton = new JButton("Load Tape");
-        JLabel tapeLabel = new JLabel("TEST");
+        loadTapeButton.addActionListener(this::loadTapeActionListener);
+        tapeLabel = new JLabel("Tape needs to be loaded");
+        tapeLabel.setForeground(Color.RED);
         tapePanel.add(loadTapeButton);
         tapePanel.add(tapeLabel);
         
         // Load Transitions button and label
         JPanel transitionsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton loadTransitionsButton = new JButton("Load Transitions");
-        JLabel transitionsLabel = new JLabel("TEST");
+        loadTransitionsButton.addActionListener(this::loadTransitionsActionListener);
+        transitionsLabel = new JLabel("Transitions need to be loaded");
+        transitionsLabel.setForeground(Color.RED);
         transitionsPanel.add(loadTransitionsButton);
         transitionsPanel.add(transitionsLabel);
         
         // Start button (disabled)
         JPanel startPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JButton startButton = new JButton("Start!");
+        startButton = new JButton("Start!");
         startButton.setEnabled(false);
+        startButton.addActionListener(this::startButtonActionListener);
         startPanel.add(startButton);
+
+        // Start with example data button
+        JButton startWithExampleButton = new JButton("Start with example data");
+        startWithExampleButton.addActionListener(this::startWithExampleActionListener);
+        startPanel.add(startWithExampleButton);
         
         // Add vertical glue to center content vertically
         mainPanel.add(Box.createVerticalGlue());
@@ -43,16 +63,75 @@ public class StartupWindow extends JFrame {
         mainPanel.add(Box.createVerticalGlue());
         
         add(mainPanel);
-    }
-
-    public void showWindow() {
         setVisible(true);
     }
-    
+
+    private void loadTapeActionListener(ActionEvent e) {
+        // Open file chooser, for now just println the selected file path
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select Tape File");
+
+        int returnValue = fileChooser.showOpenDialog(this);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            // TODO: Implement tape loading logic
+            System.out.println("Selected tape file: " + selectedFile.getAbsolutePath()); //!TEST
+
+            // When tape is loaded successfully
+            isTapeLoaded = true;
+            tapeLabel.setText("Tape loaded");
+            tapeLabel.setForeground(Color.GREEN);
+
+            // Enable start button if both tape and transitions are loaded
+            maybeEnableStartButton();
+        }
+    }
+
+    private void loadTransitionsActionListener(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select Transitions File");
+
+        int returnValue = fileChooser.showOpenDialog(this);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            // TODO: Implement transitions loading logic
+            System.out.println("Selected transitions file: " + selectedFile.getAbsolutePath()); //!TEST
+
+            // When transitions are loaded successfully
+            areTransitionsLoaded = true;
+            transitionsLabel.setText("Transitions loaded");
+            transitionsLabel.setForeground(Color.GREEN);
+
+            // Enable start button if both tape and transitions are loaded
+            maybeEnableStartButton();
+        }
+    }
+
+    private void maybeEnableStartButton() {
+        if (isTapeLoaded && areTransitionsLoaded) {
+            startButton.setEnabled(true);
+        } else {
+            startButton.setEnabled(false);
+        }
+    }
+
+    private void startButtonActionListener(ActionEvent e) {
+        // TODO: Implement start button logic
+        System.out.println("Start button clicked");
+
+        //setVisible(false);
+    }
+
+    private void startWithExampleActionListener(ActionEvent e) {
+        // TODO: Implement logic to start with example data
+        System.out.println("Start with example data button clicked");
+
+        //setVisible(false);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             StartupWindow startupWindow = new StartupWindow();
-            startupWindow.showWindow();
         });
     }
 }
